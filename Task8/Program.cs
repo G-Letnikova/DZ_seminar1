d4963d302bf1f9bff9acffd3215a80662cc9267c
GLetnikova <g.letnikova@gmail.com>
2022-11-29 15:22:46 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,18 @@
+﻿// вводим число (N), выводим все четные от 1 до N
+
+Console.WriteLine("введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+while (N<=1)
+{
+    Console.WriteLine($"{N} некорректное число, введите число, больше 1");
+    N = Convert.ToInt32(Console.ReadLine());
+
+}
+
+for (int i=2; i <= N; i=i+2) 
+   {
+     Console.Write($"{i} ");
+   }
+
+
