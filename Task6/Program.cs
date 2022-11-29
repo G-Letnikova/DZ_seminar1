7f80704634f162e007ac59d3135ddd9a7897f2d8
GLetnikova <g.letnikova@gmail.com>
2022-11-29 14:55:03 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// вводим число, выводим четоне оно или нет
+
+Console.WriteLine("введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N%2 == 0) 
+{
+    Console.WriteLine($"да");
+}
+
+else 
+{
+    Console.WriteLine($"нет");
+}
