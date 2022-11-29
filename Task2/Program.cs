e05beb0e4f4475db4d100a5c7118de8d6c1b5bce
GLetnikova <g.letnikova@gmail.com>
2022-11-29 14:29:04 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// вводим два числа, выводим какое большее
+
+Console.WriteLine("введите число 1: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число 2: ");
+int N2 = Convert.ToInt32(Console.ReadLine());
+
+if (N1 > N2) 
+{
+    Console.WriteLine($"max = {N1}");
+}
+
+else 
+{
+    Console.WriteLine($"max = {N2}");
+}
