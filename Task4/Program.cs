f75e5cdb24591cd8a883f386d51273b265ff2937
GLetnikova <g.letnikova@gmail.com>
2022-11-29 14:49:59 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿// вводим три числа, выводим максимальное
+
+Console.WriteLine("введите число 1: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число 2: ");
+int N2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число 3: ");
+int N3 = Convert.ToInt32(Console.ReadLine());
+
+if (N1 > N2 && N1 > N3) 
+{
+    Console.WriteLine($"max = {N1}");
+}
+
+    else if (N2 > N3)
+        {
+          Console.WriteLine($"max = {N2}");
+        }
+ else 
+    {
+        Console.WriteLine($"max = {N3}");
+
+     }
+            
